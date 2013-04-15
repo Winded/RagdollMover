54d0c8b8d3ef6fb94b9c50e432cced1a30a85c5b
Winded <hytonen-a@hotmail.com>
2013-04-15 12:55:10 +0300

    Added some documentation, added physbone adding code to skeleton

--- src/lua/autorun/skeleton.lua
+++ src/lua/autorun/skeleton.lua
@@ -8,30 +8,53 @@ SKNODE.__index = SKNODE;
 -- SKELETON
 ---------------
 
--- A model bone
-local NODE_TYPE_BONE = 1;
--- A physbone of a ragdoll
-local NODE_TYPE_PHYSBONE = 2;
--- Simple GetPos() and GetAngles() of the entity
-local NODE_TYPE_ORIGIN = 3;
+local NodeType =
+{
+	-- A model bone
+	Bone = 1,
+	-- A physbone of a ragdoll
+	Physbone = 2,
+	-- Simple GetPos() and GetAngles() of the entity
+	Origin = 3
+};
 
 function SK.Create(entity)
 	
 	local sk = {};
 	setmetatable(sk, SK);
-	sk:Initialize(entity);
+	
+	sk.m_Entity = entity;
+	sk.m_Nodes = {};
 	
 	if entity:GetClassName() == "prop_ragdoll" then
 		
-		--TODO
+		--Physical bones
+		for i = 0, entity:GetPhysicsObjectCount() - 1 do
+			local p = rgm.GetPhysBoneParent(entity, i);
+			local phys = entity:GetPhysicsObjectNum(i);
+			
+			local pos, ang = phys:GetPos(), phys:GetAngles();
+			
+			if p then
+				local parentphys = entity:GetPhysicsObjectNum(p);
+				local ppos, pang = parentphys:GetPos(), parentphys:GetAngles();
+				local lpos, lang = WorldToLocal(pos, ang, ppos, pang);
+				sk:CreateNode(i, p, NodeType.Physbone, i, lpos, lang);
+			else
+				sk:CreateNode(i, -1, NodeType.Physbone, i, pos, ang);
+			end
+		end
+		
+		--TODO non-physical bones? fingers? toes?
 		
 	else
-		sk:CreateNode(0, NODE_TYPE_ORIGIN, 0, entity:GetPos(), entity:GetAngles());
+		--Create only root node when not a ragdoll.
+		sk:CreateNode(0, -1, NodeType.Origin, 0, entity:GetPos(), entity:GetAngles());
 	end
 	
 end
 
-function SK:Initialize(entity)
+function SK:Initialize()
 	self.m_Entity = entity;
 	self.m_Nodes = {};
 end
@@ -45,8 +68,16 @@ function SK:SetEntity(entity)
 end
 
 function SK:CreateNode(id, parentId, type, boneId, pos, ang)
-	local node = SKNODE.Create(self, id, type, boneId, pos, ang);
-	table.insert(self.m_Nodes, node);
+
+	if self.m_Nodes[id] ~= nil then
+		error("Bone with id " .. id .. " already exists on skeleton of " .. self:GetEntity());
+	end
+	
+	local node = SKNODE.Create(self, id, parentId, type, boneId, pos, ang);
+	self.m_Nodes[id] = node;
+	
+	return node;
+	
 end
 
 _G["Skeleton"] = SK;
@@ -55,17 +86,23 @@ _G["Skeleton"] = SK;
 -- SKELETON NODE
 ---------------
 
-function SKNODE.Create(skeleton, id, type, boneId, pos, ang)
+function SKNODE.Create(skeleton, id, parentId, type, boneId, pos, ang)
 	
 	local node = {};
 	setmetatable(node, SKNODE);
-	node:Initialize(skeleton, id, type, boneId, pos, ang);
+	
+	node.m_Id = id;
+	node.m_ParentId = parentId;
+	node.m_Type = type;
+	node.m_BoneId = boneId;
+	node.m_Pos = pos;
+	node.m_Ang = ang;
 	
 	return node;
 	
 end
 
-function SKNODE:Initialize(skeleton, id, type, boneId, pos, ang)
+function SKNODE:Initialize()
 	
 end
 
