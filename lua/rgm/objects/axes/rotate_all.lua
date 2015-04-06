ffcf610409f991592bcb78aa93e4a2e50d28ab13
Winded <hytonen-a@hotmail.com>
2015-04-06 12:02:44 +0300

    RotateFaced axis now working. RotateAll axis still wip

--- lua/rgm/objects/axes/rotate_all.lua
+++ lua/rgm/objects/axes/rotate_all.lua
@@ -9,31 +9,81 @@ function AXIS.Create(gizmo, direction)
 	return AXIS.CreateBase("RotateAll", gizmo, direction);
 end
 
+function AXIS:OnGrab()
+
+	RGM.AxisBase.OnGrab(self);
+
+	local eyePos, eyeAngles = RGM.GetEyePosAng(self.Player);
+	local pos, angles = self:GetPos(), self:GetAngles();
+	local intersect = RGM.IntersectRayWithPlane(pos, angles:Forward(), eyePos, eyeAngles:Forward());
+
+	self.LastIntersect = intersect;
+
+end
+
 function AXIS:OnGrabUpdate()
-	-- TODO
+
+	local data = self.Player.RGMData;
+	local bone = RGM.GetSelectedBone(self.Player);
+	local eyePos, eyeAngles = RGM.GetEyePosAng(self.Player);
+	local pos, angles = self:GetPos(), self:GetAngles();
+	local bPos, bAngles = bone:GetPos(), bone:GetAngles();
+
+	local intersect = RGM.IntersectRayWithPlane(pos, angles:Forward(), eyePos, eyeAngles:Forward());
+	local lastIntersect = self.LastIntersect;
+	local pivotPoint = (lastIntersect + (intersect - lastIntersect) / 2) + eyeAngles:Forward() * (data.Scale / 2);
+	local lastIntersectAngle = (lastIntersect - pivotPoint):Angle();
+	local intersectAngle = (intersect - pivotPoint):Angle();
+
+	local _, lLastIntersectAngle = WorldToLocal(bPos, lastIntersectAngle, bPos, bAngles);
+	local _, lIntersectAngle = WorldToLocal(bPos, intersectAngle, bPos, bAngles);
+	local angDiff = lIntersectAngle - lLastIntersectAngle;
+	local newAngleNormal = bAngles:Forward();
+	newAngleNormal:Rotate(angDiff);
+	bone:SetAngles(newAngleNormal:Angle());
+
+	self.LastIntersect = intersect;
+
 end
 
 function AXIS:GetColor()
-	-- TODO
-end
-
-function AXIS:GetBounds()
-
-	-- TODO
-
+	return Color(100, 100, 100, 255);
 end
 
 function AXIS:IsTraceHit(intersect)
-	-- TODO
+
+	local player = self.Player;
+	local pos, angles = self:GetPos(), self:GetAngles();
+
+	local maxDistance = player.RGMData.Scale;
+
+	local distance = intersect:Distance(pos);
+	return distance <= maxDistance;
+
 end
 
 function AXIS:Draw(highlight)
 
-	if not highlight then
+	local color = self:GetColor();
+	if highlight then
+		color = Color(200, 200, 0);
+	end
+
+	local pos, angles = self:GetPos(), self:GetAngles();
+	local scale = self.Player.RGMData.Scale;
+
+	local screenPos = pos:ToScreen();
+	if not screenPos.visible then
 		return;
 	end
 
-	-- TODO
+	local width = scale;
+	local height = scale;
+	screenPos.x = screenPos.x - width / 2;
+	screenPos.y = screenPos.y - height / 2;
+
+	surface.SetDrawColor(color);
+	surface.DrawRect(screenPos.x, screenPos.y, width, height);
 
 end
 
--- lua/rgm/objects/axes/rotate_faced.lua
+++ lua/rgm/objects/axes/rotate_faced.lua
@@ -1,39 +1,46 @@
 
-local AXIS = setmetatable({}, RGM.AxisBase);
+local AXIS = setmetatable({}, RGM.AxisRotate);
 AXIS.__index = AXIS;
 
 AXIS.Mode = RGM.GizmoModes.Rotate;
 AXIS.Priority = 2;
 
+-- Build a disc from lines
+AXIS.DrawLines = {};
+local lineCount = 32;
+local rotateAngle = Angle(0, 0, 360 / lineCount);
+local startNormal = Vector(0, 0, 1.1);
+for i = 1, lineCount do
+	local startPos = startNormal * 1;
+	local endPos = startNormal * 1;
+	startPos:Rotate(rotateAngle * (i - 1));
+	endPos:Rotate(rotateAngle * i);
+	table.insert(AXIS.DrawLines, {Start = startPos, End = endPos});
+end
+
 function AXIS.Create(gizmo, direction)
 	return AXIS.CreateBase("RotateFaced", gizmo, direction);
 end
 
-function AXIS:OnGrabUpdate()
-	-- TODO
-end
-
 function AXIS:GetColor()
-	-- TODO
+	return Color(150, 150, 150, 255);
 end
 
-function AXIS:GetBounds()
-
-	-- TODO
-
+function AXIS:GetAngles()
+	return self.Player:EyeAngles();
 end
 
 function AXIS:IsTraceHit(intersect)
-	-- TODO
-end
 
-function AXIS:Draw(highlight)
+	local player = self.Player;
+	local pos, angles = self:GetPos(), self:GetAngles();
 
-	if not highlight then
-		return;
-	end
+	local scale = player.RGMData.Scale * 1.1;
+	local thickness = scale * 0.1;
+	local minDistance, maxDistance = scale - thickness / 2, scale + thickness / 2;
 
-	-- TODO
+	local distance = intersect:Distance(pos);
+	return distance >= minDistance and distance <= maxDistance;
 
 end
 
